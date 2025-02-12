bab0179bc5e72d52e4c15759ef71c284bdc54556
mohammed3200 <wwyuu799@gmail.com>
2025-02-12 12:00:39 +0200

    Create a validation scheme for Admin form and Battalions form

--- renderer/lib/validation.ts
+++ renderer/lib/validation.ts
@@ -4,11 +4,12 @@ export const UserFormValidation = z.object({
     fullName: z.string().min(3,"اسم قصير جدا"),
     nationalNumber: z.string().min(12,"هذا ليس رقم وطني").max(12,"هذا ليس رقم وطني"),
     nationality: z.string(),
-    birthDate: z.coerce.date(),
+    birthDate: z.string().default(`${new Date()}`),
     PlaceOfBirth:z.string().min(2,"هذا قصير جدا").optional(),
     phone: z
     .string()
     .refine((phone) => /^\d{3}-\d{6,10}$/.test(phone), "ليس رقم هاتف"),
+    battalionName: z.string().min(1,"هذا قصير جدا").max(40,"الاسم طويل جدا"),
     TypeOfDefinition : z.enum(["ID card","Passport"]),
     battalionNumber: z.string().optional(),
     IDCard: z.string().min(6,"هذا ليس رقم تعريف").max(6,"هذا ليس رقم تعريف").optional(),
@@ -26,4 +27,13 @@ export const UserFormValidation = z.object({
 export const AdminFormValidation = z.object({
     username: z.string().min(1,"مطلوب"),
     password: z.string().min(1,"مطلوب"),
-})
+});
+
+export const BattalionsFormValidation = z.object({
+    battalionName: z.string().min(1,"مطلوب"),
+    place: z.string().min(1,"مطلوب"),
+    conductor: z.string().min(1,"مطلوب"),
+    dateOfCreation: z.string().default(`${new Date()}`),
+    numberOfIndividuals: z.number().default(0),
+    weaponsType: z.string().trim().optional(),
+});

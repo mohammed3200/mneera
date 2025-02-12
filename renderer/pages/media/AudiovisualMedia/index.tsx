59611e40f69708d2c818542ef4606f20cc2f3c6c
mohammed3200 <wwyuu799@gmail.com>
2025-02-12 18:53:52 +0200

    Create a dashboard media

---
+++ renderer/pages/media/AudiovisualMedia/index.tsx
@@ -0,0 +1,13 @@
+import { RootLayout, HeaderTitle } from '@/renderer/components';
+
+const page = () => {
+    return (
+        <RootLayout>
+            <HeaderTitle title="الاعلام المرئي و المسموع" back="/media" />
+            <div className="flex justify-between px-2 mt-4 items-center">
+
+            </div>
+        </RootLayout>
+    );
+};
+export default page;
---
+++ renderer/pages/media/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { RootLayout, HeaderTitle } from '@/renderer/components';
+import { Button } from '@/renderer/components/ui/button';
+
+const branch = [
+    {
+        title: "الاعلام المرئي و المسموع",
+        route: "/AudiovisualMedia"
+    },
+    {
+        title: "التواصل الاجتماعي",
+        route: "/SocialCommunication"
+    },
+    {
+        title: "الناطق الرسمي",
+        route: "/OfficialSpokesperson"
+    }
+]
+
+const page = () => {
+    const router = useRouter();
+
+    return (
+        <RootLayout>
+            <HeaderTitle title="الاعلام" back="/dashboard" />
+            <div className="flex justify-evenly px-2 mt-4 items-center gap-6 h-[50vh]">
+                {
+                    branch.map((item) => (
+                        <Button
+                            onClick={() => router.push(`/media/${item.route}`)}
+                            className="w-full py-4 border border-gray-200 text-gray-300 rounded-lg
+         bg-transparent hover:bg-blue-700 hover:border-none items-center justify-center mt-5
+         justify-self-center self-center duration-500 transition-all ease-out"
+                        >
+                            <p className="font-din-bold text-md text-right ">
+                                {item.title}
+                            </p>
+                        </Button>
+                    ))
+                }
+            </div>
+        </RootLayout>
+    );
+};
+
+export default page;

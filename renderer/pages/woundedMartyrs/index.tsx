2151cfb1cc828e930ba9a08e2ab5c9c6d7198427
mohammed3200 <wwyuu799@gmail.com>
2025-02-12 18:55:32 +0200

    Create a dashboard wounded martyrs

---
+++ renderer/pages/woundedMartyrs/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { RootLayout, HeaderTitle } from '@/renderer/components';
+import { Button } from '@/renderer/components/ui/button';
+
+const branch = [
+    {
+        title: "الشهداء",
+        route: "/Martyrs"
+    },
+    {
+        title: "الجراحة",
+        route: "/Surgery"
+    },
+]
+
+const page = () => {
+    const router = useRouter();
+
+    return (
+        <RootLayout>
+            <HeaderTitle title="الجرحى و الشهداء" back="/dashboard" />
+            <div className="flex justify-evenly px-2 mt-4 items-center gap-6 h-[50vh]">
+                {
+                    branch.map((item) => (
+                        <Button
+                            onClick={() => router.push(`/woundedMartyrs/${item.route}`)}
+                            className="w-full py-4 border border-gray-200 text-gray-300 rounded-lg
+         bg-transparent hover:bg-blue-700 hover:border-none items-center justify-center mt-5
+         justify-self-center self-center duration-500 transition-all ease-out"
+                        >
+                            <p className="font-din-bold text-md text-right ">
+                                {item.title}
+                            </p>
+                        </Button>
+                    ))
+                }
+            </div>
+        </RootLayout>
+    );
+};
+
+export default page;

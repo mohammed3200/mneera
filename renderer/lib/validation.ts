58ac9522956969cf9b7866ae899ed0b875e73448
mohammed3200 <wwyuu799@gmail.com>
2025-02-20 21:04:56 +0200

    Add WoundedFormValidation schema and enhance existing validation with blood type enum

--- renderer/lib/validation.ts
+++ renderer/lib/validation.ts
@@ -1,39 +1,79 @@
 import { z } from "zod";
+import { blood } from "../types/constants";
+
 
 export const UserFormValidation = z.object({
-    fullName: z.string().min(3,"اسم قصير جدا"),
-    nationalNumber: z.string().min(12,"هذا ليس رقم وطني").max(12,"هذا ليس رقم وطني"),
-    nationality: z.string(),
-    birthDate: z.string().default(`${new Date()}`),
-    PlaceOfBirth:z.string().min(2,"هذا قصير جدا").optional(),
-    phone: z
+  fullName: z.string().min(3, "اسم قصير جدا"),
+  nationalNumber: z
+    .string()
+    .min(12, "هذا ليس رقم وطني")
+    .max(12, "هذا ليس رقم وطني"),
+  nationality: z.string(),
+  birthDate: z.string().default(`${new Date()}`),
+  PlaceOfBirth: z.string().min(2, "هذا قصير جدا").optional(),
+  phone: z
     .string()
     .refine((phone) => /^\d{3}-\d{6,10}$/.test(phone), "ليس رقم هاتف"),
-    battalionName: z.string().min(1,"هذا قصير جدا").max(40,"الاسم طويل جدا"),
-    TypeOfDefinition : z.enum(["ID card","Passport"]),
-    battalionNumber: z.string().optional(),
-    IDCard: z.string().min(6,"هذا ليس رقم تعريف").max(6,"هذا ليس رقم تعريف").optional(),
-    passport:z.string().min(9,"هذا ليس رقم جواز سفر").max(9,"هذا ليس رقم جواز سفر").optional(),
-    employer: z.string(),
-    academic:z.string(),
-    weapon : z.string(),
-    blood: z.enum(["+O","-O","+A","-A","+B","-B","+AB","-AB"]),
-    address:z.string().min(2,"العنوان قصير جدا"),
-    selfie: z.custom<File[]>((value) => Array.isArray(value), "يجب أن يكون مصفوفة").default(() => [
-        new File([""], "logo.png", { type: "image/png", lastModified: Date.now() })
-    ]).optional(),
+  battalionName: z.string().min(1, "هذا قصير جدا").max(40, "الاسم طويل جدا"),
+  TypeOfDefinition: z.enum(["ID card", "Passport"]),
+  battalionNumber: z.string().optional(),
+  IDCard: z
+    .string()
+    .min(6, "هذا ليس رقم تعريف")
+    .max(6, "هذا ليس رقم تعريف")
+    .optional(),
+  passport: z
+    .string()
+    .min(9, "هذا ليس رقم جواز سفر")
+    .max(9, "هذا ليس رقم جواز سفر")
+    .optional(),
+  employer: z.string(),
+  academic: z.string(),
+  weapon: z.string(),
+  blood: z.enum(["+O", "-O", "+A", "-A", "+B", "-B", "+AB", "-AB"]),
+  address: z.string().min(2, "العنوان قصير جدا"),
+  selfie: z
+    .custom<File[]>((value) => Array.isArray(value), "يجب أن يكون مصفوفة")
+    .default(() => [
+      new File([""], "logo.png", {
+        type: "image/png",
+        lastModified: Date.now(),
+      }),
+    ])
+    .optional(),
 });
 
 export const AdminFormValidation = z.object({
-    username: z.string().min(1,"مطلوب"),
-    password: z.string().min(1,"مطلوب"),
+  username: z.string().min(1, "مطلوب"),
+  password: z.string().min(1, "مطلوب"),
 });
 
 export const BattalionsFormValidation = z.object({
-    battalionName: z.string().min(1,"مطلوب"),
-    place: z.string().min(1,"مطلوب"),
-    conductor: z.string().min(1,"مطلوب"),
-    dateOfCreation: z.string().default(`${new Date()}`),
-    numberOfIndividuals: z.number().default(0),
-    weaponsType: z.string().trim().optional(),
+  battalionName: z.string().min(1, "مطلوب"),
+  place: z.string().min(1, "مطلوب"),
+  conductor: z.string().min(1, "مطلوب"),
+  dateOfCreation: z.string().default(`${new Date()}`),
+  numberOfIndividuals: z.number().default(0),
+  weaponsType: z.string().trim().optional(),
+});
+
+export const WoundedFormValidation = z.object({
+  name: z.string().min(1, "مطلوب"),
+  dateOfBirth: z.string().refine((date) => {
+    const parsedDate = Date.parse(date);
+    const year = new Date(parsedDate).getFullYear();
+    return !isNaN(parsedDate) && year >= 1900 && year <= 2500;
+  }, "تاريخ غير صالح"),
+  phone: z
+    .string()
+    .refine((phone) => /^\d{3}-\d{6,10}$/.test(phone), "ليس رقم هاتف"),
+  bloodType: z.nativeEnum(blood),
+  typeOfInjury: z.string(),
+  injuryLocation: z.string().min(2, "العنوان قصير جدا"),
+  injuryDescription: z.string().optional(),
+  dateOfInjury: z.string().refine((date) => {
+    const parsedDate = Date.parse(date);
+    const year = new Date(parsedDate).getFullYear();
+    return !isNaN(parsedDate) && year >= 1900 && year <= 2500;
+  }, "تاريخ غير صالح"),
 });

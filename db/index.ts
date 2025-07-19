203adf008c41253ef9600232019b42b0153da6d6
mohammed3200 <wwyuu799@gmail.com>
2025-07-19 13:41:02 +0200

    `feat: updated codebase with IPC and Next.js configuration changes`

--- db/index.ts
+++ db/index.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 
-const dbPath = "C:/db/sqlite.db";
+const dbPath = "./sqlite.db";
 const sqlite = new Database(dbPath);
 export const db = drizzle(sqlite, { schema });

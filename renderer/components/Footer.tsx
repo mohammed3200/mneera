787a6f2767ec881298c32c350e8c5997366e2b73
mohammed3200 <wwyuu799@gmail.com>
2025-07-19 13:41:30 +0200

    `Refactor: Update preload handler, Next.js config, and individuals page to use IPC and fetch data from main process.`

--- renderer/components/Footer.tsx
+++ renderer/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Footer = () => {
   return (
     <div className='w-full flex flex-row items-center justify-center bg-transparent pb-4'>
--- renderer/components/Header.tsx
+++ renderer/components/Header.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react'
 import Image from 'next/image'
 
 

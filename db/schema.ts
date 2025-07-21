61c852a709268a20b781f0b2da6b6e5481715c50
Cit2024 <expo@cit.edu.ly>
2025-07-21 20:44:21 +0200

    feat: add image storage and individual identifiers
    - Introduce a new images table to store binary image data, type, and size.
    - Add an image_id column to the individuals table to link individuals to their associated images.
    - Include a passport_number column in the individuals table for an alternative unique identifier.
    - Make the id_number column in the individuals table optional to accommodate cases where only a passport number is provided

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer, blob } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
@@ -11,9 +11,11 @@ export const users = sqliteTable("users", {
 export const individuals = sqliteTable("individuals", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
+  image: text("image_id").notNull(),
   nationalNumber: text("national_number").notNull().unique(),
   birthDate: integer("birth_date", { mode: "number" }).notNull(),
-  idNumber: text("id_number").notNull().unique(),
+  idNumber: text("id_number").unique(),
+  passportNumber : text("passport_number").unique(),
   address: text("address").notNull(),
   placeOfBirth: text("place_of_birth").notNull(),
   battalion: text("battalion").notNull(),
@@ -26,3 +28,13 @@ export const individuals = sqliteTable("individuals", {
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
 });
+
+export const images = sqliteTable('Image', {
+  id: integer('id').primaryKey(),      // use cuid() in your application for default
+  data: blob('data'),                // BLOB in SQLite
+  type: text('type'),                // MIME type or custom type string
+  size: integer('size'),             // file size in bytes
+  createdAt: integer('created_at', { mode: "timestamp" }).default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: integer('updated_at', { mode: "timestamp" }).default(sql`CURRENT_TIMESTAMP`),
+});
+
